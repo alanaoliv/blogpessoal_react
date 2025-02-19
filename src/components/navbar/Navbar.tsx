fa2eaf161308228b90605292759e3f81e08ed6c0
alanaoliv <alanasanches97@gmail.com>
2025-02-19 08:26:24 -0300

    :sparkles: CRUD completo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+    
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+    
   return (
     <>
         <div className='
@@ -29,22 +43,21 @@ function Navbar() {
                     justify-around
                     pl-8
                     pr-8
-                    font-[Monomakh-xs]
                     font-semibold
                 '>
                     <Link to="/postagem">
                     Postagens
                     </Link>
-                    <Link to="/tema"> 
+                    <Link to='/temas' className='hover:underline'> 
                     Temas
                     </Link>
-                    <Link to="/cadastrar">       
+                    <Link to='/cadastrartema' className='hover:underline'>       
                     Cadastrar tema
                     </Link> 
                     <Link to="/perfil">
                     Perfil 
                     </Link>
-                    <Link to="/login">
+                    <Link to='' onClick={logout} className='hover:underline'>
                         Sair 
                     </Link>
                 </div>
